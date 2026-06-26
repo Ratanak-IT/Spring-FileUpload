d343aa76decdc98774a5fa09caeff03bf07fe9c5
Thai Ratanak.IT <ratanakcoding.it@gmail.com>
2026-06-26 19:23:34 +0700

    ASS03

--- src/main/java/org/example/datajpa/features/order/dto/SoftDeleteRequest.java
+++ src/main/java/org/example/datajpa/features/order/dto/SoftDeleteRequest.java
@@ -2,7 +2,7 @@ package org.example.datajpa.features.order.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record SoftDeleteRequest(
-        @NotNull(message = "status is required")
+        @NotNull(message = "delete is required")
         Boolean isDelete
 ) {
 }

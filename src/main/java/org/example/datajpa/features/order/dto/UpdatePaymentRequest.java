f353eba498c2bcb0fd2f144f8585d181450e10eb
Thai Ratanak.IT <ratanakcoding.it@gmail.com>
2026-06-26 19:22:48 +0700

    ASS03

--- src/main/java/org/example/datajpa/features/order/dto/UpdatePaymentRequest.java
+++ src/main/java/org/example/datajpa/features/order/dto/UpdatePaymentRequest.java
@@ -1,6 +1,8 @@
 package org.example.datajpa.features.order.dto;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdatePaymentRequest(
+        @NotNull(message = "status is required")
         Boolean status
 ) {
 }
